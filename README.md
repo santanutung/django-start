98e61c67abecf2048938aa3da3984bc02652775c
santanutung <santanuroyzzzz@gmail.com>
2021-04-28 12:27:53 +0530

    heroku setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-"# django-start" 
+"# django-start"
 # django-start
+pip install gunicorn
+
+pip  freeze > requirements.txt
+
+
+import  django_heroku
+import dj_database_url
+
+django_heroku.settings(locals())
+
--- testproject/settings.py
+++ testproject/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+import  django_heroku
+import dj_database_url
+from pathlib import Path
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
